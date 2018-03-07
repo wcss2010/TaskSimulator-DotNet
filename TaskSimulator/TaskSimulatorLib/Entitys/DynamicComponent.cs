1b9026982fcd6a5c19d78cb6c01fdb0dc5fb8310
wcss2010 <wcss2020@gmail.com>
2018-03-07 16:05:58 +0800

    。。。

---
+++ TaskSimulator/TaskSimulatorLib/Entitys/DynamicComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSimulatorLib.Entitys
+{
+    /// <summary>
+    /// 用于定义机器人动态组件
+    /// </summary>
+    public class DynamicComponent
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public string ComponentId { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string ComponentName { get; set; }
+
+        /// <summary>
+        /// 类全名
+        /// </summary>
+        public string ComponentClassFullName { get; set; }
+
+        /// <summary>
+        /// 类文件
+        /// </summary>
+        public string ComponentClassFile { get; set; }
+    }
+}
---
+++ TaskSimulator/TaskSimulatorLib/Entitys/RobotSimulatorConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskSimulatorLib.Util;
+
+namespace TaskSimulatorLib.Entitys
+{
+    /// <summary>
+    /// 机器人模拟器配置
+    /// </summary>
+    [Serializable]
+    public class RobotSimulatorConfig
+    {
+        public RobotSimulatorConfig()
+        {
+            Robots = new Robot[0];
+            SocketController = new DynamicComponent();
+            MonitorComponentMap = new SerializableDictionary<string, DynamicComponent>();
+            TaskComponentMap = new SerializableDictionary<string, DynamicComponent>();
+        }
+
+        /// <summary>
+        /// 机器人列表
+        /// </summary>
+        public Robot[] Robots { get; set; }
+
+        /// <summary>
+        /// Socket控制器
+        /// </summary>
+        public DynamicComponent SocketController { get; set; }
+
+        /// <summary>
+        /// 监视器动态组件列表(Key=MonitorId,Value=.cs定义)
+        /// </summary>
+        public SerializableDictionary<string, DynamicComponent> MonitorComponentMap { get; set; }
+
+        /// <summary>
+        /// 任务动态组件列表(Key=TaskId,Value=.cs定义)
+        /// </summary>
+        public SerializableDictionary<string, DynamicComponent> TaskComponentMap { get; set; }
+    }
+}
