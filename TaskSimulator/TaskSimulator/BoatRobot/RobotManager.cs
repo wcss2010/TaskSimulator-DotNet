26e4f4ced1ebf02cfeb5dcf304b2261973b672bb
wcss2010 <wcss2020@gmail.com>
2018-03-07 09:03:34 +0800

    增加配置文件加载和保存项目...

--- TaskSimulator/TaskSimulator/BoatRobot/RobotManager.cs
+++ TaskSimulator/TaskSimulator/BoatRobot/RobotManager.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using TaskSimulator.BoatRobot.Entitys;
 
 namespace TaskSimulator.BoatRobot
 {
@@ -14,6 +17,48 @@ namespace TaskSimulator.BoatRobot
     /// </summary>
     public class RobotManager
     {
+        /// <summary>
+        /// 配置文件名称
+        /// </summary>
+        public const string ROBOT_CONFIG_FILENAME = "config.xml";
 
+        private static RobotSimulatorConfig simulatorConfig = null;
+        /// <summary>
+        /// 机器人配置文件
+        /// </summary>
+        public static RobotSimulatorConfig SimulatorConfig
+        {
+            get { return RobotManager.simulatorConfig; }
+            set { RobotManager.simulatorConfig = value; }
+        }
+
+        /// <summary>
+        /// 载入配置文件
+        /// </summary>
+        public static void LoadConfig()
+        {
+            SimulatorConfig = XmlSerializeTool.Deserialize<RobotSimulatorConfig>(File.ReadAllText(Path.Combine(Application.StartupPath, ROBOT_CONFIG_FILENAME)));
+        }
+
+        /// <summary>
+        /// 保存配置文件
+        /// </summary>
+        public static void SaveConfig()
+        {
+            if (SimulatorConfig == null)
+            {
+                return;
+            }
+
+            string xml = XmlSerializeTool.Serializer<RobotSimulatorConfig>(SimulatorConfig);
+            if (string.IsNullOrEmpty(xml))
+            {
+                return;
+            }
+            else
+            {
+                File.WriteAllText(Path.Combine(Application.StartupPath, ROBOT_CONFIG_FILENAME), xml);
+            }
+        }
     }
 }
